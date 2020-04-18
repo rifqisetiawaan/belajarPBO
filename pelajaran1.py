77638abedcda525f07b84e59e55d5e0229db98f2
rifqisetiawaan <rifqisetiawaan12@gmail.com>
2020-04-18 21:51:36 +0700

    penambahan atribut "attack_diterima"

--- pelajaran1.py
+++ pelajaran1.py
@@ -1,21 +1,23 @@
 class Hero:
     def __init__(self, name, health, attackPower, armor):
-        self.nama = name
+        self.name = name
         self.health = health
         self.attack = attackPower
         self.armor = armor
 
     def printNama(self):
-        print("nama Saya adalah " + self.nama + " darah saya " + str(self.health))
+        print("nama Saya adalah " + self.name + " darah saya " + str(self.health))
         print("serangan saya " + str(self.attack) + " armor saya " + str(self.armor))
         print("\n")
 
     def serang(self, lawan):
-        print(self.nama + " menyerang " + lawan.nama)
+        print(self.name + " menyerang " + lawan.nama)
         lawan.diserang(self)
 
-    def diserang(self, lawan):
-        print(self.nama + " diserang " + lawan.nama)
+    def diserang(self, lawan, attackPower_lawan):
+        print(self.name + " diserang " + lawan.nama)
+        attack_diterima = attackPower_lawan / self.armor
+        print("Serangan Terasa " + str(attack_diterima))
 
 
 sniper = Hero("Sniper", 100, 50, 5)
