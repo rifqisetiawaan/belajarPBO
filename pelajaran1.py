ec45bec92d3b24410f354c744cc00cf3485788f1
rifqisetiawaan <rifqisetiawaan12@gmail.com>
2020-04-18 16:32:54 +0700

    penambahan atribut armor

--- pelajaran1.py
+++ pelajaran1.py
@@ -1,17 +1,21 @@
 class Hero:
-    def __init__(self, name, health, attackPower):
+    def __init__(self, name, health, attackPower, armor):
         self.nama = name
         self.health = health
         self.attack = attackPower
+        self.armor = armor
 
     def printNama(self):
-        print("nama Saya adalah " + self.nama + " darah saya " + str(self.health) + " serangan saya " + str(self.attack))
+        print("nama Saya adalah " + self.nama + " darah saya " + str(self.health))
+        print("serangan saya " + str(self.attack) + " armor saya " + str(self.armor))
+        print("\n")
 
     def serang(self, lawan):
         print(self.nama + " menyerang " + lawan.nama)
 
-sniper = Hero("Sniper", 100, 50)
-marksmen = Hero("Marksmen", 100, 50)
+
+sniper = Hero("Sniper", 100, 50, 5)
+marksmen = Hero("Marksmen", 100, 50, 5)
 
 sniper.printNama()
 marksmen.printNama()
