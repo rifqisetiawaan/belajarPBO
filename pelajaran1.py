ac42b963bf3ab630870d52b9393f6a74bd5df469
rifqisetiawaan <rifqisetiawaan12@gmail.com>
2020-04-18 16:24:42 +0700

    Penambahan atribut "attackPower"

--- pelajaran1.py
+++ pelajaran1.py
@@ -1,13 +1,14 @@
 class Hero:
-    def __init__(self, name, health):
+    def __init__(self, name, health, attackPower):
         self.nama = name
         self.health = health
+        self.attack = attackPower
 
     def printNama(self):
-        print("nama Saya adalah " + self.nama + " darah saya " + str(self.health))
+        print("nama Saya adalah " + self.nama + " darah saya " + str(self.health) + " serangan saya " + str(self.attack))
 
-sniper = Hero("Sniper", 100)
-marksmen = Hero("Marksmen", 100)
+sniper = Hero("Sniper", 100, 50)
+marksmen = Hero("Marksmen", 100, 50)
 
 sniper.printNama()
 marksmen.printNama()
